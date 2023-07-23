c3fc010a2dfede8f6564be455b10cf8d4c002f2b
Mohammed Farooq basha S <sfarooq8.fb@gmail.com>
2023-07-23 22:17:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,42 @@ def map_back(column, mapping):
     else:
         return column
 
+# Custom CSS styles for the app
+st.markdown(
+    """
+    <style>
+    .header {
+        background-color: #33adff;
+        color: white;
+        padding: 0.5rem;
+        text-align: center;
+        font-size: 2rem;
+        border-radius: 0.5rem;
+        margin-bottom: 1rem;
+    }
+    .button {
+        background-color: #33adff;
+        color: white;
+        padding: 0.5rem 1rem;
+        font-size: 1rem;
+        border: none;
+        border-radius: 0.5rem;
+        cursor: pointer;
+    }
+    .prediction {
+        font-size: 1.5rem;
+        font-weight: bold;
+        margin-top: 1rem;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Streamlit app
 def main():
-    st.title('Insurance Subscription Prediction')
+    st.title('Customer Insurance Subscription Prediction')
+    st.markdown('<p class="header">Customer Details</p>', unsafe_allow_html=True)
 
     # Read the dataset
     dataset = pd.read_csv('train.csv')
@@ -24,41 +57,24 @@ def main():
 
     # Map categorical features to numeric values
     mapping = {
-        'y': {'no': 0, 'yes': 1},
-        'mon': {'may': 5, 'jun': 6, 'jul': 7, 'aug': 8, 'oct': 10, 'nov': 11, 'dec': 12, 'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4, 'sep': 9},
-        'education_qual': {'tertiary': 3, 'secondary': 2, 'unknown': 0, 'primary': 1},
-        'marital': {'married': 24, 'single': 16, 'divorced': 32},
-        'call_type': {'unknown': 0, 'cellular': 1, 'telephone': 2},
-        'prev_outcome': {'unknown': 2, 'failure': 0, 'other': 3, 'success': 1}
+        # ... (your mapping code here)
     }
     dataset.replace(mapping, inplace=True)
 
     # Encode 'job' feature using LabelEncoder
     le = LabelEncoder()
     dataset['job'] = le.fit_transform(dataset['job'])
-    
+
     # Inverse transform function to map numeric values back to job names
     def inverse_transform_job(code):
         return le.inverse_transform([code])[0]
 
     # Get user inputs for features
-    st.subheader('Enter the Customer Details:')
     job = st.selectbox('Job', dataset['job'].unique(), format_func=inverse_transform_job)
-    marital = st.selectbox('Marital Status', dataset['marital'].unique(), format_func=lambda x: map_back(pd.Series([x]), mapping['marital'])[0])
-    education_qual = st.selectbox('Education Qualification', dataset['education_qual'].unique(), format_func=lambda x: map_back(pd.Series([x]), mapping['education_qual'])[0])
-    call_type = st.selectbox('Call Type', dataset['call_type'].unique(), format_func=lambda x: map_back(pd.Series([x]), mapping['call_type'])[0])
-    prev_outcome = st.selectbox('Previous Outcome', dataset['prev_outcome'].unique(), format_func=lambda x: map_back(pd.Series([x]), mapping['prev_outcome'])[0])
-    mon = st.selectbox('Month', dataset['mon'].unique(), format_func=lambda x: map_back(pd.Series([x]), mapping['mon'])[0])
-    age = st.slider('Age', min_value=0, max_value=100, value=30)
-    day = st.slider('Day', min_value=1, max_value=31, value=1)
-    dur = st.slider('Duration', min_value=0, max_value=1000, value=200)
-    num_calls = st.slider('Number of Calls', min_value=0, max_value=20, value=5)
+    # ... (rest of your code)
 
     # Add a "Predict" button
-    predict_button = st.button('Predict')
-
-    # Make prediction when the "Predict" button is pressed
-    if predict_button:
+    if st.button('Predict', key='predict_button'):
         # Prepare the input data for prediction
         data = pd.DataFrame({
             'job': [job],
@@ -77,11 +93,11 @@ def main():
         prediction = model.predict(data)[0]
 
         # Display the prediction
-        st.subheader('Prediction:')
+        st.markdown('<p class="prediction">Prediction:</p>', unsafe_allow_html=True)
         if prediction == 0:
-            st.write('No, The customer will not subscribe to the insurance.')
+            st.write('No, The customer is highly unlikely to subscribe to the insurance.')
         else:
-            st.write('Yes, The customer will subscribe to the insurance.')
+            st.write('Yes, The customer is highly likely to subscribe to the insurance.')
 
 # Run the app
 if __name__ == '__main__':
