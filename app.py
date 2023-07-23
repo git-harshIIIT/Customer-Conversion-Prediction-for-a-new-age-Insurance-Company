ba7d1d5e4649ad33fe13f2987513d7c57c485169
Mohammed Farooq basha S <sfarooq8.fb@gmail.com>
2023-07-23 22:23:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,9 +55,15 @@ def main():
     dataset = pd.read_csv('train.csv')
     dataset = dataset.dropna(axis=0)
 
+
     # Map categorical features to numeric values
     mapping = {
-        # ... (your mapping code here)
+        'y': {'no': 0, 'yes': 1},
+        'mon': {'may': 5, 'jun': 6, 'jul': 7, 'aug': 8, 'oct': 10, 'nov': 11, 'dec': 12, 'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4, 'sep': 9},
+        'education_qual': {'tertiary': 3, 'secondary': 2, 'unknown': 0, 'primary': 1},
+        'marital': {'married': 24, 'single': 16, 'divorced': 32},
+        'call_type': {'unknown': 0, 'cellular': 1, 'telephone': 2},
+        'prev_outcome': {'unknown': 2, 'failure': 0, 'other': 3, 'success': 1}
     }
     dataset.replace(mapping, inplace=True)
 
@@ -68,11 +74,19 @@ def main():
     # Inverse transform function to map numeric values back to job names
     def inverse_transform_job(code):
         return le.inverse_transform([code])[0]
-
     # Get user inputs for features
+    st.subheader('Enter the Customer Details:')
     job = st.selectbox('Job', dataset['job'].unique(), format_func=inverse_transform_job)
-    # ... (rest of your code)
-
+    marital = st.selectbox('Marital Status', dataset['marital'].unique(), format_func=lambda x: map_back(pd.Series([x]), mapping['marital'])[0])
+    education_qual = st.selectbox('Education Qualification', dataset['education_qual'].unique(), format_func=lambda x: map_back(pd.Series([x]), mapping['education_qual'])[0])
+    call_type = st.selectbox('Call Type', dataset['call_type'].unique(), format_func=lambda x: map_back(pd.Series([x]), mapping['call_type'])[0])
+    prev_outcome = st.selectbox('Previous Outcome', dataset['prev_outcome'].unique(), format_func=lambda x: map_back(pd.Series([x]), mapping['prev_outcome'])[0])
+    mon = st.selectbox('Month', dataset['mon'].unique(), format_func=lambda x: map_back(pd.Series([x]), mapping['mon'])[0])
+    age = st.slider('Age', min_value=0, max_value=100, value=30)
+    day = st.slider('Day', min_value=1, max_value=31, value=1)
+    dur = st.slider('Duration ', min_value=0, max_value=1000, value=200)
+    num_calls = st.slider('Number of Calls', min_value=0, max_value=20, value=5)
+    
     # Add a "Predict" button
     if st.button('Predict', key='predict_button'):
         # Prepare the input data for prediction
