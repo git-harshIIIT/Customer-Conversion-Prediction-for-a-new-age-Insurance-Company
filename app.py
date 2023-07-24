8ca3f665e48ef25be4d812691f964b92bfdfbb47
Mohammed Farooq basha S <sfarooq8.fb@gmail.com>
2023-07-24 10:01:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import pandas as pd
-import joblib
-from sklearn.preprocessing import LabelEncoder
 
 # Load the trained model
 model = joblib.load('model.pkl')
@@ -43,7 +40,6 @@ st.markdown(
         font-weight: bold;
         margin-top: 1.5rem;
         text-align: center;
-
     }
     .footer {
         text-align: center;
@@ -51,6 +47,14 @@ st.markdown(
     .branding {
         font-size: 1rem;
         font-weight: bold;
+    }
+
+    .cover-image {
+        max-width: 75%;
+        margin: 0 auto;
+        display: block;
+        border-radius: 1rem;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
     }
 
     .sidebar-content {
@@ -81,9 +85,7 @@ def main():
 
     # Load the image from the same GitHub repository where app.py is located
     image_path = 'mlcus.jpg'
-    image = st.image(image_path, use_column_width=True)
-
-
+    image = st.image(image_path, use_column_width=True, caption='')
 
     # Sidebar menu
     st.sidebar.title('Navigation')
